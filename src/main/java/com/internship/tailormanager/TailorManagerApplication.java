ab94746fefd715ec0bab871d1691e06a0cee7d97
Dharma Raj Thanait <dharmarajthanait25@gmail.com>
2021-10-17 07:52:20 +0545

    Created basic project structure

---
+++ src/main/java/com/internship/tailormanager/TailorManagerApplication.java
@@ -0,0 +1,13 @@
+package com.internship.tailormanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailorManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TailorManagerApplication.class, args);
+    }
+
+}
